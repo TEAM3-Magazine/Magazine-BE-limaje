bb4d3970746c4fffbb5cf7c71ac1a434b8d7a1f7
limjae <limjae96@gmail.com>
2022-04-20 22:02:54 +0900

    4/20 Enabling CORS...

--- PBL2/pbl2/src/main/java/com/pbl2/pbl2/security/CorsConfig.java
+++ PBL2/pbl2/src/main/java/com/pbl2/pbl2/security/CorsConfig.java
@@ -16,7 +16,7 @@ public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOriginPattern("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.addExposedHeader("Authorization");
