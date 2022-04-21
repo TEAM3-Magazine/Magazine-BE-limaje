fbfbc235d4d1a0542c3e71b6bac445dcad667e86
limjae <limjae96@gmail.com>
2022-04-21 22:41:28 +0900

    4/21 Server Time Zone Setting

--- PBL2/pbl2/src/main/java/com/pbl2/pbl2/Pbl2Application.java
+++ PBL2/pbl2/src/main/java/com/pbl2/pbl2/Pbl2Application.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Pbl2Application {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pbl2Application.class, args);
